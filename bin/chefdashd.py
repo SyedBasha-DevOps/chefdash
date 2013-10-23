38a6f81b463b31e93dc93269c4c055b9c344d8fc
Evan Todd <et1337@gmail.com>
2013-10-23 16:39:50 -0400

    Fix stupid errors

--- bin/chefdashd.py
+++ bin/chefdashd.py
@@ -8,19 +8,20 @@ if __name__ == '__main__':
 	import logging
 	import sys
 
-	if sys.argc > 1:
+	if len(sys.argv) > 1:
 		chefdash.app.config.from_pyfile(sys.argv[1])
 
 	host = 'localhost'
 	port = 5000
 
-	server_name = chefdash.app.config['SEVER_NAME']
-	if ':' in server_name:
+	server_name = chefdash.app.config.get('SERVER_NAME')
+	if server_name is not None and ':' in server_name:
 		server_name = server_name.split(':')
 		host = server_name[0]
 		port = int(server_name[1])
 
-	logging.basicConfig(filename = chefdash.app.config['LOG_FILE'], level = logging.INFO)
+	if not chefdash.app.debug:
+		logging.basicConfig(filename = chefdash.app.config['LOG_FILE'], level = logging.INFO)
 
 	server = gevent.pywsgi.WSGIServer((host, port), chefdash.handler, handler_class = geventwebsocket.handler.WebSocketHandler)
 	server.serve_forever()
